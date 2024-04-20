ba2181bdba2f49749e9452ef59497be7e0a06a61
s13ad <s13ad@MSI>
2024-04-20 21:35:13 +0100

    Add project files.

---
+++ Advanced Programming 1/FakeFileSystem.cpp
@@ -0,0 +1,1 @@
+#include "FakeFileSystem.h"
---
+++ Advanced Programming 1/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+#ifdef _DEBUG
+	_onexit(_CrtDumpMemoryLeaks);
+#endif
+
+	// ask for root dir path //
+	cout << "Enter root directory path: ";
+	getline(cin, initialDirectoryPath);
+	
+	// const rootDir
+
+	return 0;
+}
